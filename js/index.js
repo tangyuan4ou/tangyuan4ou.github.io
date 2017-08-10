abb03829470a5dd09e9c1463c6c8a71d118af30f
tangyuan4ou <tangyuan_youyou@163.com>
2017-08-10 13:25:47 +0800

    Site updated: 2017-08-10 13:25:47

--- js/index.js
+++ js/index.js
@@ -1,15 +1,41 @@
-/*
-  获取页面高度并设置首页bg高
-*/
-var height = window.innerHeight;
-var width = window.innerWidth;
-$('.indexBg').css('height', height );
-$('.title').css('line-height', height + 'px');
-
-/*
-  首页canvas
-*/
 (function() {
+  /*
+    获取页面宽高度
+  */
+  var height = window.innerHeight;
+  var width = window.innerWidth;
+
+  /*
+    浏览器宽度小于1200
+  */
+  $('.error').removeClass('errorOn');
+  $('.error2').removeClass('errorOn');
+  if (width < 1200) {
+    $('.main').remove();
+    $('.error2').remove();
+    $('.error').addClass('errorOn');
+  }
+
+  /*
+    判断浏览器种类
+  */
+  var userAgent = navigator.userAgent; //获取浏览器userAgent字符串
+  console.log(userAgent)
+  if (userAgent.indexOf('Firefox') < -1 && userAgent.indexOf('Chrome') < -1 && userAgent.indexOf('Safari') < -1) {
+    $('.main').remove();
+    $('.error').remove();
+    $('.error2').addClass('errorOn');
+  }
+    
+  /*
+    设置首页bg高
+  */
+  $('.indexBg').css('height', height );
+  $('.title').css('line-height', height + 'px');
+
+  /*
+    首页canvas
+  */
 
   var  largeHeight, canvas, ctx, cirles, target, animateHeader = true;
 
@@ -34,6 +60,7 @@ $('.title').css('line-height', height + 'px');
   }
 
   function Circle() {
+    console.log(this);
     var _this = this;
     (function(){
       _this.pos = {};
@@ -43,20 +70,20 @@ $('.title').css('line-height', height + 'px');
     function init() {
       _this.pos.x = Math.random() * width;
       _this.pos.y = height + Math.random()*100;
-      _this.alpha = 0.1 + Math.random()*0.3;
+      _this.alpha = 0.8 + Math.random()*0.3;
       _this.scale = 0.1 + Math.random()*0.3;
       _this.velocity = Math.random();
     }
     
     this.draw = function() {
-      if(_this.alpha <= 0) {
+      if(_this.alpha <= 0.3) {
         init();
       }
       _this.pos.y -= _this.velocity;
-      _this.alpha -= 0.0005;
+      _this.alpha -= 0.001;
       ctx.beginPath();
       ctx.arc(_this.pos.x, _this.pos.y, _this.scale*10, 0, 2*Math.PI, false);
-      ctx.fillStyle = 'rgba(255,255,255' + _this.alpha + ')';
+      ctx.fillStyle = 'rgba(0,0,0,' + _this.alpha + ')';
       ctx.fill();
     };
   }
@@ -68,39 +95,56 @@ $('.title').css('line-height', height + 'px');
   $('.menuContainer').css('height', height);
   $('.menuBg').css('height', height);
   $('.menu').css('height', height);
-  $('.list').css('width', newWidth/4);
-  $('.list').css('height', height);
-  $('.list').css('line-height', height + 'px');
+  $('.list').css({'width': newWidth/4, 'height': height, 'line-height': height + 'px'});
 
-  // $('.about').css('left', -500);
-  // $('.img').css('left', -500);
-  // $('.blog').css('left', -500);
-  // $('.contact').css('left', -500);
 
   $('.about').css('left', 0);
   $('.img').css('left', newWidth-newWidth/4*3);
   $('.blog').css('left', newWidth-newWidth/4*2);
   $('.contact').css('left', newWidth-newWidth/4);
 
-  $(".menu-toggle").on('click', function() {
-    $(this).toggleClass("on");
-    $('.menu-section').toggleClass("on");
-    $('.menuContainer').toggleClass("menuContainer2");
-    $('.contact').toggleClass("fadeInUp");
-    setTimeout(function(){
-      $('.blog').toggleClass("fadeInUp");
-    },500);
-    setTimeout(function(){
-      $('.img').toggleClass("fadeInUp");
-    },1000);
-    setTimeout(function(){
-      $('.about').toggleClass("fadeInUp");
-    },1500);
+  /*
+    菜单呼入和呼出
+  */
+  $('.menu-toggle').toggle(
+    function() {
+      $('.menu-toggle').addClass('on');
+      $('.menu-section').addClass('on');
+      $('.name').text('');
+      $('.menuContainer').addClass('menuContainer2');
+      $('.list').removeClass("fadeInDown");
+      $('.contact').addClass("fadeInUp");
+      setTimeout(function(){
+        $('.blog').addClass("fadeInUp");
+      },500);
+      setTimeout(function(){
+        $('.img').addClass("fadeInUp");
+      },1000);
+      setTimeout(function(){
+        $('.about').addClass("fadeInUp");
+      },1500);
+    },
+    function() {
+      $('.menu-toggle').removeClass('on');
+      $('.menu-section').removeClass('on');
+      $('.about').removeClass("fadeInUp").addClass("fadeInDown");
+      setTimeout(function(){
+        $('.img').removeClass("fadeInUp").addClass("fadeInDown");
+      },500);
+      setTimeout(function(){
+        $('.blog').removeClass("fadeInUp").addClass("fadeInDown");
+      },1000);
+      setTimeout(function(){
+        $('.contact').removeClass("fadeInUp").addClass("fadeInDown");
+        $('.menuContainer').addClass('menuDown');
+      },1500);
+      setTimeout(function(){
+        $('.menuContainer').removeClass('menuContainer2', 'menuDown');
+        $('.name').text('Hi friend');
+      },2000);
+    }
+  );
 
-    
 
-    // $('li:eq(0)').toggleClass("about");
-    // $('li:eq(0)').toggleClass("about-out");
-  });
 
 })();
